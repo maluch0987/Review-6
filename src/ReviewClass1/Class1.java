21f489434a81a77736d53eeb3531e961fcbbe0ec
malush0987 <qa0150783@gmail.com>
2022-06-09 22:32:26 -0400

    2nd commit

---
+++ src/ReviewClass1/Class1.java
@@ -0,0 +1,13 @@
+package ReviewClass1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+private void sysy() {
+	// TODO Auto-generated method stub
+System.out.println("mam");
+}
+}
